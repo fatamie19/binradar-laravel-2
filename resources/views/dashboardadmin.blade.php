4d8302bc9b134d4944c49a2368271969fea7f448
fatamie19 <22ftt1507@student.pb.edu.bn>
2024-08-09 23:39:33 +0800

    Update page titles and headers for aboutus, analysis, and notifications views

--- resources/views/dashboardadmin.blade.php
+++ resources/views/dashboardadmin.blade.php
@@ -39,8 +39,21 @@
             </div>
           </div>
 
-          <a href="{{ route('addnewbin') }}" class="bg-white border-border border-2 shadow-md rounded-lg p-4 mt-auto"> + Add new rubbish bin</a>
-          
+        <!-- New Bin -->
+            <div class="bg-white flex flex-col items-center p-4 border border-border rounded-lg shadow-md bg-card w-64 sm:w-80 lg:w-96">
+              <img aria-hidden="true" alt="kitchen-bin" src="https://openui.fly.dev/openui/100x100.svg?text=" class="w-20 h-20"/>
+              <span class="mt-2 font-semibold">New</span>
+              <span class="text-muted-foreground p-2">New</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="size-6 text-green">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+              </svg>
+
+            </div>
+          </div>
+        
+            <div class="flex justify-center">
+                <a href="{{ route('addnewbin') }}" class="bg-white border-border border-2 shadow-md rounded-lg p-4 mt-auto"> + Add new rubbish bin</a>
+            </div>
         </div>
       </body>
     </html>
