ebd8f451b137c0f2066d65dc95d90927d765cae5
Akmal-Dan <akmaldanish3054@gmail.com>
2024-08-18 20:23:52 +0800

    - Updating Dashboard file. - Updating Dashboard-bindetail file.

--- resources/views/dashboard-bindetail.blade.php
+++ resources/views/dashboard-bindetail.blade.php
@@ -6,23 +6,33 @@
     </x-slot>
 
 
-    <div id="right-side" >
-{{--        style="display: none"--}}
-
         <div class="absolute" style="height: 300px; width: 400px; top: 250px; left: 200px">
+
+{{--            bin title --}}
             <h1 class="flex justify-center font-bold" style="font-size: 30px">Example</h1>
-            <div class="relative" style="height: 40px; width: 200px; top: 40px; margin-left: auto; margin-right: auto;">
-                <button class="shadow" style="height: 40px; width: 200px; background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px;">Change</button>
+
+{{--            change button --}}
+            <div class="relative flex justify-center" style="height: 40px; top: 40px;">
+                <a href="{{route('dashboard')}}" class="shadow" style="width: 200px; height: 40px; background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px; text-align: center;">Change</a>
             </div>
-            <div class="relative shadow" style="height: 200px; width: 400px; top: 60px; margin-left: auto; margin-right: auto;"></div>
+
+{{--            status--}}
+            <div class="relative shadow rounded-2xl" style="height: 200px; width: 400px; top: 60px; margin-left: auto; margin-right: auto;">
+                <div class="max-w-max rounded-t-2xl" style="background-color: #4CAF50; text-align: center; padding-top: 10px; padding-bottom: 10px;">
+                    <p class="text-white">Date & Time</p>
+                    <p class="text-white">...</p>
+                </div>
+                <p class="text-3xl" style="text-align: center; padding-top: 30px;">
+                    Bin level capacity is low
+                </p>
+
+            </div>
+
         </div>
 
-        <div class="absolute shadow" style="height: 500px; width: 500px; top: 200px; right: 250px"></div>
+{{--    level capacity --}}
+        <div class="absolute shadow" style="height: 500px; width: 500px; top: 200px; right: 250px">
 
-        <a href="dashboard" class="relative" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; margin-bottom: 10px; top: 20px; left: 20px;">
-            Back
-        </a>
-
-    </div>
+        </div>
 
 </x-app-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
 
-    <div class="py-12" id="left-side">
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
             <h1 class="flex justify-center relative text-3xl font-bold text-black" style="">My Rubbish Bins</h1>
@@ -14,7 +14,7 @@
 
 
             <div class="flex justify-center" style="padding-top: 50px">
-                <a href="dashboard-bindetail" class="p-4 rounded-md text-sm font-semibold text-black shadow" style="width: 800px; text-align: center">EXAMPLE</a>
+                <a href="dashboard-bindetail" class="p-4 rounded-md text-sm font-semibold text-black shadow" style="width: 800px; text-align: center">Living Room Bin</a>
             </div>
 
             <br>
