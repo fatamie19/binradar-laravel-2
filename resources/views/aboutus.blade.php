91f3714e977f6211e8525e1d1d40d03ddb4612c3
fatamie19 <22ftt1507@student.pb.edu.bn>
2024-08-19 18:44:32 +0800

    Update aboutus.blade.php

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -5,9 +5,17 @@
         </h2>
     </x-slot>
 
-        <h1 class="flex justify-center relative text-3xl" style="top: 100px; color: #16a34a">Bin Radar</h1>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com?plugins=forms,typography"></script>
+        <script src="https://unpkg.com/unlazy@0.11.3/dist/unlazy.with-hashing.iife.js" defer init></script>
+      </head>
+
+        <h1 class="flex justify-center relative text-2xl" style="top: 100px; color: #16a34a">Bin Radar</h1>
         <div style="width: 50px; height: 50px;"></div>
-        <p class="flex justify-center relative text-2xl" style="top: 130px; text-align: center;" >
+        <p class="flex justify-center relative text-xl" style="top: 80px; text-align: center;" >
 
             Welcome to BinRadar! We are on a mission to <br>
             transfer waste management using cutting edge IoT Technology <br>
@@ -16,4 +24,5 @@
             Join us in our journey towards a cleaner, greener future!
         </p>
 
+    </html>
 </x-app-layout>
